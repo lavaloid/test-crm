1baf6ca5f1ed6e74db53a59a5822ae41ba22582b
lavaloid <36921951+lavaloid@users.noreply.github.com>
2023-06-03 18:57:03 +0800

    create list page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,68 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
 
-export default function Home() {
+import { Client, ClientInput, StatusType, useClientDatabase } from "@/data";
+import { Button, Card, Overlay } from "@blueprintjs/core";
+import { useMemo, useState } from "react";
+
+import { CreateForm, ListFilters, Table } from "@/components";
+import styles from "./page.module.css";
+
+const Page = () => {
+  const [formOpen, setFormOpen] = useState<boolean>(false);
+
+  const [statusFilter, setStatusFilter] = useState<string | undefined>();
+  const [startDate, setStartDate] = useState<Date | undefined>();
+  const [endDate, setEndDate] = useState<Date | undefined>();
+
+  const { getMany, insert, loaded } = useClientDatabase();
+
+  const filterFunction = (client: Client) => {
+    if (
+      statusFilter &&
+      statusFilter !== "NONE" &&
+      client.status !== statusFilter
+    )
+      return false;
+
+    if (startDate && startDate > client.dateCreated) return false;
+    if (endDate && endDate < client.dateCreated) return false;
+
+    return true;
+  };
+  const clientData = getMany(filterFunction);
+
+  const onCreate = (data: ClientInput) => insert({ ...data, status: "ACTIVE" });
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div>
+      <div className={styles.filterContainer}>
+        <ListFilters
+          {...{
+            statusFilter,
+            setStatusFilter,
+            startDate,
+            setStartDate,
+            endDate,
+            setEndDate,
+          }}
         />
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={styles.tableHeader}>
+        <Button icon="add" intent="primary" onClick={() => setFormOpen(true)}>
+          Add Client
+        </Button>
+        <Overlay isOpen={formOpen} onClose={() => setFormOpen(false)}>
+          <Card title="Create Client">
+            <CreateForm
+              onClose={() => setFormOpen(false)}
+              onSubmit={onCreate}
+            />
+          </Card>
+        </Overlay>
       </div>
-    </main>
-  )
-}
+      <Table data={clientData} loaded={loaded} />
+    </div>
+  );
+};
+
+export default Page;
